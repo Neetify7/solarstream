e5e5120d662b8d3c8f68722d82045d33d8a392ac
Neetify7 <220405487+Neetify7@users.noreply.github.com>
2025-07-19 12:28:04 -0400

    Revert "Added banner carousel"
    This reverts commit 492bd873ecc3dac1d448b85706662272c2983968.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,46 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import BannerCarousel from './components/banner';
-import Nav from './components/nav';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+function GitHub() {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <FontAwesomeIcon
+      icon={faGithub}
+      size="lg"
+      style={{
+        color: hover ? '#ff5d5d' : '#e1e1e1',
+        transition: 'color 0.2s ease',
+        fill: 'currentColor',
+      }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    />
+  );
+}
 
 export default function MyApp() {
   return (
     <>
-      <Nav/>
-      <BannerCarousel/>
+      <nav>
+        <div className="nav-left">
+          <Link href="/">home</Link>
+          <Link href="/movie">movie</Link>
+          <Link href="/tv">tv</Link>
+          <Link href="/anime">anime</Link>
+        </div>
+        <div className="nav-mid">
+          <Link href="/">solarstream</Link>
+        </div>
+        <div className="nav-right">
+          <Link href="https://github.com/Neetify7/solarstream" target="_blank"><GitHub/></Link>
+        </div>
+      </nav>
+      <div className="center">
+        <p>hello</p>
+      </div>
     </>
   );
 }
