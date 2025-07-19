b80693bedaea84fa427e23873c76feec0c02de0d
Neetify7 <220405487+Neetify7@users.noreply.github.com>
2025-07-19 12:33:51 -0400

    Converted navbar into reusable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 'use client'
-import React from 'react';
-import { useState } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import Nav from './components/nav';
 
 function GitHub() {
   const [hover, setHover] = useState(false);
@@ -28,20 +25,7 @@ function GitHub() {
 export default function MyApp() {
   return (
     <>
-      <nav>
-        <div className="nav-left">
-          <Link href="/">home</Link>
-          <Link href="/movie">movie</Link>
-          <Link href="/tv">tv</Link>
-          <Link href="/anime">anime</Link>
-        </div>
-        <div className="nav-mid">
-          <Link href="/">solarstream</Link>
-        </div>
-        <div className="nav-right">
-          <Link href="https://github.com/Neetify7/solarstream" target="_blank"><GitHub/></Link>
-        </div>
-      </nav>
+      <Nav/>
       <div className="center">
         <p>hello</p>
       </div>
